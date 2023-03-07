88578ea6445a2d521005db98199cef877dec6ec0
unknown <mziyyah010@gmail.com>
2023-03-07 04:57:54 -0800

    updated

--- js/script.js
+++ js/script.js
@@ -1,4 +1,20 @@
+// const btn = document.getElementById('menu-btn')
+// let nav = document.getElementById('menu')
 
+// btn.addEventListener('click', () => {
+//     btn.classList.toggle('open')
+//     nav.classList.toggle('flex')
+//     nav.classList.toggle('hidden')
+//     nav.style.right = "0"
+// })
+
+let checkmenu = document.getElementById("menu")  
+	function showMenu(){
+		checkmenu.style.right = "0px";
+	}
+	function hideMenu(){
+		checkmenu.style.right = "-700px";
+	}
 
 
 
